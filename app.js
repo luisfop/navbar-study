e280140edd17e666cf1d76c8861b4491f6beac51
luisop <luisfop77@gmail.com>
2020-11-09 10:09:01 -0300

    finished animated navbar

--- app.js
+++ app.js
@@ -2,25 +2,42 @@ const sections = document.querySelectorAll('section');
 const bubble = document.querySelector('.bubble');
 
 const gradients = [
-    'background: linear-gradient(to right, #2c3e50, #bdc3c7)',
-    'background: linear-gradient(to right, #4286f4, #373B44)',
-    'background: -webkit-linear-gradient(to right, #4A00E0, #8E2DE2)'
+    'linear-gradient(to right, #2c3e50, #bdc3c7)',
+    'linear-gradient(to right, #4286f4, #373B44)',
+    'linear-gradient(to right, #4A00E0, #8E2DE2)',
 ];
 
 const options = {
     treshHold: 0.7,
 }
+
 let observer = new IntersectionObserver(navCheck, options);
-
-
 
 function navCheck(entries) {
     entries.forEach(entry => {
-        console.log(entry);
+        const className = entry.target.className;
+        const activeAnchor = document.querySelector(`[data-page=${className}]`);
+        const gradientIndex = entry.target.getAttribute("data-index");
+        const coords = activeAnchor.getBoundingClientRect();
+        const directions = {
+            height: coords.height,
+            width: coords.width,
+            top: coords.top,
+            left: coords.left,
+        }
+        if(entry.isIntersecting){
+            bubble.style.setProperty('left', `${ directions.left}px`);
+            bubble.style.setProperty('top', `${ directions.top}px`); 
+            bubble.style.setProperty('width', `${ directions.width}px`); 
+            bubble.style.setProperty('height', `${ directions.height}px`); 
+            bubble.style.background = gradients[gradientIndex];
+            
+        }
+        
     })
 }
 
 sections.forEach(section => {
     observer.observe(section)
-    console.log('Observing');
+    // console.log('Observing');
 })
